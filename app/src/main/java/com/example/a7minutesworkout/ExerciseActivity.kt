bfcc609216a96d38f156cd504e5861b611c920f5
HanSeokMin <tjrals604@naver.com>
2023-05-11 13:16:41 +0900

    다음 운동 알림 기능 추가

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -54,11 +54,16 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.tvExerciseName?.visibility = View.INVISIBLE
         binding?.flExerciseView?.visibility = View.INVISIBLE
         binding?.ivImage?.visibility = View.INVISIBLE
+        binding?.tvUpcomingLabel?.visibility = View.VISIBLE
+        binding?.tvUpComingExerciseName?.visibility = View.VISIBLE
 
         if (restTimer != null) {
             restTimer!!.cancel()
             restProgress = 0
         }
+
+        binding?.tvUpComingExerciseName?.text =
+            exerciseList!![currentExercisePosition + 1].getName()
         setRestProgressBar()
     }
 
@@ -68,7 +73,8 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.tvExerciseName?.visibility = View.VISIBLE
         binding?.flExerciseView?.visibility = View.VISIBLE
         binding?.ivImage?.visibility = View.VISIBLE
-
+        binding?.tvUpcomingLabel?.visibility = View.INVISIBLE
+        binding?.tvUpComingExerciseName?.visibility = View.INVISIBLE
 
         // 초기화
         if (exerciseTimer != null) {
